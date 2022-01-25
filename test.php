658f30eb26afc22727fcb63e0c8f1b0f60124f72
Gngzs <48579703+Gngzs@users.noreply.github.com>
2022-01-25 21:56:03 +0800

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,6 @@
 if(isset($_POST['action'])){
             
             if($_POST['action'] == "guangnian"){
-                //echo "密码正确";
-                //header("www.baidu.com");
                 echo "location.href = 'http://www.baidu.com'";
             }else{
                 echo "alert('密码错误')";
