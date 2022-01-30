81276139d47a732e0d5fcb74f7f7428b113eed77
Gngzs <48579703+Gngzs@users.noreply.github.com>
2022-01-30 17:53:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "112.213.124.230:3306";
-$username = "jiemi";
-$password = "love1314%";
-$dbname = "jiemi";
+$servername = "数据库地址，本地为localhost";
+$username = "数据库用户名";
+$password = "数据库密码";
+$dbname = "数据库名";
 $url="http(s)://xxx.xxx.xxx/"
 ?>
