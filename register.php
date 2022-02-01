134cea613ac513216c15cb9936c6e6b0cc433874
Gngzs <48579703+Gngzs@users.noreply.github.com>
2022-02-01 20:10:13 +0800

    Update register.php

--- register.php
+++ register.php
@@ -24,7 +24,7 @@ if (!isset($b)) {
             $iipp = $_SERVER["REMOTE_ADDR"];
             $uuid = md5($_COOKIE['mail']);
             $conn = mysqli_connect($servername, $username, $password, $dbname);
-            $sql2 = "INSERT INTO `user` (`un`, `pw`,`mail`,`code`,`uuid`,`ip`) VALUES ('" . $_COOKIE['username'] . "', '" . $_COOKIE['password'] . "','" . $_COOKIE['mail'] . "','" . $_COOKIE['code'] . "','" . $uuid . "','" . $iipp . "')";
+            $sql2 = "INSERT INTO `user` (`un`, `pw`,`mail`,`code`,`uuid`,`ip`) VALUES ('" . $_COOKIE['username'] . "', '" . md5($_COOKIE['password']) . "','" . $_COOKIE['mail'] . "','" . $_COOKIE['code'] . "','" . $uuid . "','" . $iipp . "')";
             mysqli_query($conn, $sql2);
             mysqli_close($conn);
             echo "<script>alert('注册成功！');location.href='index.html'</script>";
